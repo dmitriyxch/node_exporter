baf1355a4c08ac1216d12c11ddcb6586bb587fe5
Dmitriy <66207986+dmitriyxch@users.noreply.github.com>
2021-10-05 23:12:42 +0300

    Create node_exporter.sh

---
+++ node_exporter.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#wget -q -O node_exporter.sh https://gist.githubusercontent.com/dmitriyxch/5bf96793edb03a5b1931a8e572b334b7/raw/166df0a9fd987c03ef6edbcae7ae0c322f36b245/node_exporter.sh && chmod +x node_exporter.sh && sudo /bin/bash node_exporter.sh
+
+version="${VERSION:-1.2.2}"
+arch="${ARCH:-linux-amd64}"
+bin_dir="${BIN_DIR:-/usr/local/bin}"
+
+wget "https://github.com/prometheus/node_exporter/releases/download/v$version/node_exporter-$version.$arch.tar.gz" -O /tmp/node_exporter.tar.gz
+
+mkdir -p /tmp/node_exporter
+
+cd /tmp || { echo "ERROR! No /tmp found.."; exit 1; }
+
+tar xfz /tmp/node_exporter.tar.gz -C /tmp/node_exporter || { echo "ERROR! Extracting the node_exporter tar"; exit 1; }
+
+cp "/tmp/node_exporter/node_exporter-$version.$arch/node_exporter" "$bin_dir"
+chown root:staff "$bin_dir/node_exporter"
+
+cat <<EOF > /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Prometheus node exporter
+After=local-fs.target network-online.target network.target
+Wants=local-fs.target network-online.target network.target
+[Service]
+Type=simple
+#ExecStartPre=-/sbin/iptables -I INPUT 1 -p tcp --dport 9100 -s 127.0.0.1 -j ACCEPT
+#ExecStartPre=-/sbin/iptables -I INPUT 3 -p tcp --dport 9100 -j DROP
+ExecStart=/usr/local/bin/node_exporter
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable node_exporter.service
+systemctl start node_exporter.service
+systemctl status node_exporter.service
+
+theIPaddress=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+echo $theIPaddress":9100"
+echo "SUCCESS! Installation succeeded!"
