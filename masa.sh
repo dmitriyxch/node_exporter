e217c98b149b399b1f450b63eede156642281f3d
Nonce <66207986+dmitriyxch@users.noreply.github.com>
2022-04-09 21:20:17 +0300

    Update masa.sh

--- masa.sh
+++ masa.sh
@@ -1,44 +1,36 @@
-sudo apt install apt-transport-https net-tools git mc sysstat atop curl tar wget clang pkg-config libssl-dev jq build-essential make ncdu -y
-sudo addgroup p2p 
-sudo adduser masa --ingroup p2p --disabled-password --disabled-login --shell /usr/sbin/nologin --gecos ""
+sudo apt install apt-transport-https net-tools git mc sysstat atop curl tar wget clang pkg-config libssl-dev 
 ver="1.17.5"
 cd ~
 wget --inet4-only "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-sleep 5
-echo "1"
+
 rm "go$ver.linux-amd64.tar.gz"
-echo "2"
+
 echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> ~/.profile
-echo "3"
+
 source ~/.profile
-echo "4"
-echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> /home/masa/.profile
-echo "5"
-sleep 5
-echo "6"
-sudo su masa
-echo "7"
-sleep 5
-cd ~
-echo "8"
+
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> ~/masa/.profile
+
+
+
 source ~/.profile
-echo "9"
+
 git clone https://github.com/masa-finance/masa-node-v1.0
-echo "10"
+
 cd masa-node-v1.0/src
 make all
-exit
-sudo -i 
-cp /home/masa/masa-node-v1.0/src/build/bin/* /usr/local/bin
-exit
-sudo su masa -s /bin/bash
+
+
+cp ~/masa/masa-node-v1.0/src/build/bin/* /usr/local/bin
+
+
 cd ~
 source ~/.profile
 cd $HOME/masa-node-v1.0
 geth --datadir data init ./network/testnet/genesis.json
-exit
+
 sudo -i
 read NODE_NAME
 tee /etc/systemd/system/masad.service > /dev/null <<EOF
@@ -47,7 +39,7 @@ Description=MASA
 After=network.target
 [Service]
 Type=simple
-User=masa
+User=root
 ExecStart=/usr/local/bin/geth \\
 --identity ${NODE_NAME} \\
 --datadir /home/masa/masa-node-v1.0/data \\
@@ -73,7 +65,7 @@ Environment="PRIVATE_CONFIG=ignore"
 [Install]
 WantedBy=multi-user.target
 EOF
-exit
+
 sudo systemctl daemon-reload
 sudo systemctl enable masad
 sudo systemctl restart masad
