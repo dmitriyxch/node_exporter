dcfaf512cb6215a2e13b5288f26332282f30bc2d
Nonce <66207986+dmitriyxch@users.noreply.github.com>
2022-04-19 20:11:10 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,4 +4,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce
+sudo apt install docker-compose
 sudo systemctl status docker
