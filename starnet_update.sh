20e9a680b120c0eb5c506538f4cfe5b1f619ad7c
Nonce <66207986+dmitriyxch@users.noreply.github.com>
2022-05-08 22:25:55 +0300

    Create starnet_update.sh

---
+++ starnet_update.sh
@@ -0,0 +1,19 @@
+cd ~/pathfinder 
+
+git fetch
+
+git checkout v0.1.8-alpha
+
+cargo build --release --bin pathfinder 
+
+mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
+
+cd py
+
+source .venv/bin/activate
+
+PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
+
+systemctl restart starknetd
+
+pathfinder -V
