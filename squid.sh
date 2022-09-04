0f09323f5ec605b59c4afd1ddf7ea1500ba97a91
Nonce <66207986+dmitriyxch@users.noreply.github.com>
2022-09-05 01:10:11 +0300

    Update squid.sh

--- squid.sh
+++ squid.sh
@@ -43,7 +43,7 @@ acl CONNECT method CONNECT
 
 
 #allow basic auth
-auth_param basic program /usr/lib/squid/basic_ncsa_auth /etc/squid/htpasswd
+auth_param basic program /usr/lib/squid/basic_ncsa_auth /etc/squid/passwd
 auth_param basic children 5
 auth_param basic realm Squid proxy-caching web server
 auth_param basic credentialsttl 2 hours
